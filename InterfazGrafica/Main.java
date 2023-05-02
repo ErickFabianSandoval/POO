13057e212f5bb20f8f3a58812110aa5eb44e8ead
ErickFabianSandoval <126055753+ErickFabianSandoval@users.noreply.github.com>
2023-05-01 23:27:45 -0600

    Add files via upload

---
+++ InterfazGrafica/Main.java
@@ -0,0 +1,9 @@
+package fes.ico.InterfazGrafica;
+
+import fes.ico.InterfazGrafica.iu.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+        Principal ventana = new Principal("Hola mundo AWT");
+    }
+}
