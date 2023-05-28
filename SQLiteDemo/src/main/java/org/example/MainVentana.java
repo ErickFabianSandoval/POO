602081d9f359bbf7a778decd59a9171eda86d528
ErickFabianSandoval <126055753+ErickFabianSandoval@users.noreply.github.com>
2023-05-27 21:44:19 -0600

    Add files via upload

---
+++ SQLiteDemo/src/main/java/org/example/MainVentana.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.controlador.ControladorLibro;
+import org.example.vista.VentanaLibro;
+
+public class MainVentana {
+    public static void main(String[] args) {
+        VentanaLibro view = new VentanaLibro("MVC y JDBC");
+        ControladorLibro controller = new ControladorLibro(view);
+    }
+}
